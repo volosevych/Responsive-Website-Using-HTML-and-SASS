02f622a7df59e9705597b43e3fb8962f2d401635
volosevych <vestyankas@gmail.com>
2021-05-17 14:05:11 -0600

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews-slider').slick({
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 3,
+      prevArrow: '<span class="prev-arrow"><i class="fas fa-long-arrow-alt-left"></i></span>',
+      nextArrow: '<span class="next-arrow"><i class="fas fa-long-arrow-alt-right"></i></span>'
+    });
+  });
+
