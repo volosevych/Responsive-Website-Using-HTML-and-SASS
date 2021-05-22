af6c51786741b4f5cdde3f8130b0dbef023a0d02
volosevych <vestyankas@gmail.com>
2021-05-22 17:37:27 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-$(document).ready(function(){
-    $('.reviews-slider').slick({
-      infinite: false,
-      slidesToShow: 1,
-      slidesToScroll: 3,
-      prevArrow: '<span class="prev-arrow"><i class="fas fa-long-arrow-alt-left"></i></span>',
-      nextArrow: '<span class="next-arrow"><i class="fas fa-long-arrow-alt-right"></i></span>'
-    });
+$(document).ready(function () {
+  let mainNav = document.getElementById('js-menu');
+  let navBarToggle = document.getElementById('js-navbar-toggle');
+
+  navBarToggle.addEventListener('click', function () {
+
+    mainNav.classList.toggle('active');
   });
 
+  $('.reviews-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    prevArrow: '<span class="prev-arrow"><i class="fas fa-long-arrow-alt-left"></i></span>',
+    nextArrow: '<span class="next-arrow"><i class="fas fa-long-arrow-alt-right"></i></span>'
+  });
+});
